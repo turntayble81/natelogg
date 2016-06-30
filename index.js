3c74a5b04605200eaeafdfcb3927ced9fd2be553
haigo <haig.papaghanian@gmail.com>
2016-06-30 00:38:58 -0400

    Core functionality is now complete.

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ var express  = require('express');
 var socketio = require('socket.io');
 var morgan   = require('morgan');
 var fs       = require('fs');
+var Tail     = require('tail').Tail;
 var config   = require('./config');
 var app      = express();
+var watchers = {};
+var _socket;
 
 app.set('view engine', 'ejs');
 app.use(morgan('dev'));
@@ -13,6 +16,18 @@ app.get('/', function (req, res) {
         if(err) {
            return res.send('Can\'t read log directory ' + config.logDirectory); 
         }
+        logs.forEach(function(log) {
+            var watcher = new Tail(config.logDirectory + '/' + log);
+            watcher.unwatch();
+            watcher.on('line', function(data) {
+                _socket.emit('logData', data);
+            });
+            watcher.on('error', function(err) {
+                _socket.emit('logData', data);
+            });
+            watchers[log] = watcher;
+        });
+
         res.render('main', {
             logs: logs
         });
@@ -26,6 +41,18 @@ var io = socketio(server);
 
 io.on('connection', function(socket) {
     console.log('Client websocket connected.');
+
+    _socket = socket;
+    socket.on('toggleLog', function(data) {
+        var watcher = watchers[data.log];
+        if(data.enabled) {
+            console.log('Log %s is now being watched.', data.log);
+            watcher.watch();
+        }else {
+            console.log('Log %s is no longer being watched.', data.log);
+            watcher.unwatch();
+        }
+    });
 
     socket.on('disconnect', function() {
         console.log('Client websocket disconnected.');
