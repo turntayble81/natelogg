b47135ad02da9fa6538c4c553f963f073cb28abe
haigo <haig.papaghanian@gmail.com>
2016-07-05 12:30:50 -0400

    Refactored and cleaned up log watching logic to handle multiple clients and to properly unsubscribe from logs which are no longer being watched.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express  = require('express');
-var socketio = require('socket.io');
-var morgan   = require('morgan');
-var fs       = require('fs');
-var Tail     = require('tail').Tail;
-var config   = require('./config');
-var Bunyan   = require('./formatters/bunyan');
-var app      = express();
-var watchers = {};
-var _socket;
+var express         = require('express');
+var socketio        = require('socket.io');
+var morgan          = require('morgan');
+var fs              = require('fs');
+var Tail            = require('tail').Tail;
+var config          = require('./config');
+var Bunyan          = require('./formatters/bunyan');
+var app             = express();
+var watchers        = {};
 
 var bunyan = new Bunyan({
     colorMode: 'HTML'
@@ -21,23 +20,6 @@ app.get('/', function (req, res) {
         if(err) {
            return res.send('Can\'t read log directory ' + config.logDirectory); 
         }
-        logs.forEach(function(log) {
-            var watcher = new Tail(config.logDirectory + '/' + log, {
-                fromBeginning: false,
-                follow: true
-            });
-
-            watcher.unwatch();
-            watcher.on('line', function(data) {
-                data = bunyan.handleLogLine(data);
-
-                _socket.emit('logData', data);
-            });
-            watcher.on('error', function(err) {
-                _socket.emit('logData', data);
-            });
-            watchers[log] = watcher;
-        });
 
         res.render('main', {
             logs: logs
@@ -53,19 +35,87 @@ var io = socketio(server);
 io.on('connection', function(socket) {
     console.log('Client websocket connected.');
 
-    _socket = socket;
+    console.log('Creating log event handlers');
+    socket._lineHandler = function(data) {
+        data = bunyan.handleLogLine(data);
+        socket.emit('logData', data);
+    };
+
+    socket._errHandler = function(err) {
+        socket.emit('logData', data);
+    };
+
     socket.on('toggleLog', function(data) {
-        var watcher = watchers[data.log];
+        var watcher;
+
         if(data.enabled) {
             console.log('Log %s is now being watched.', data.log);
-            watcher.watch();
+
+            if(!watchers[data.log]) {
+                console.log('Watcher does not exist for log %s. Creating now.', data.log);
+                watcher = new Tail(config.logDirectory + '/' + data.log, {
+                    fromBeginning : false,
+                    follow        : true
+                });
+
+                watcher._log          = data.log;
+                watcher._lineHandlers = [];
+                watcher._errHandlers  = [];
+
+                watcher.on('line', function(data) {
+                    watcher._lineHandlers.forEach(function(handler) {
+                        handler(data);
+                    });
+                });
+
+                watcher.on('error', function(err) {
+                    watcher._errHandlers.forEach(function(handler) {
+                        handler(err);
+                    });
+                });
+
+                watchers[data.log] = watcher;
+            }else {
+                watcher = watchers[data.log];
+            }
+
+            subscribe(watcher);
         }else {
             console.log('Log %s is no longer being watched.', data.log);
-            watcher.unwatch();
+            watcher = watchers[data.log];
+            unsubscribe(watcher);
         }
     });
 
     socket.on('disconnect', function() {
         console.log('Client websocket disconnected.');
+        console.log('Unsubscribing from all log events.');
+        Object.keys(watchers).forEach(function(watcher) {
+            watcher = watchers[watcher];
+            unsubscribe(watcher);
+        });
     });
+
+    function subscribe(watcher) {
+        console.log('Subscribing to log events for %s.', watcher._log);
+        watcher._lineHandlers.push(socket._lineHandler);
+        watcher._errHandlers.push(socket._errHandler);
+    }
+
+    function unsubscribe(watcher) {
+        var lineHandlerPos = watcher._lineHandlers.indexOf(socket._lineHandler);
+        var errHandlerPos  = watcher._errHandlers.indexOf(socket._errHandler);
+
+        console.log('Unsubscribing from log events for %s.', watcher._log);
+        if(lineHandlerPos > -1) {
+            watcher._lineHandlers.splice(lineHandlerPos, 1);
+        }
+        if(errHandlerPos > -1) {
+            watcher._errHandlers.splice(errHandlerPos, 1);
+        }
+        if(!watcher._lineHandlers.length && !watcher._errHandlers.length) {
+            console.log('No more handlers are bound to the watcher for %s. Killing watcher.', watcher._log);
+            delete watchers[watchers._log];
+        }
+    }
 });
