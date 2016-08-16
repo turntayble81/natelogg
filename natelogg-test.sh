2227b4f7bf158fd22703b3a77739d3e3c8fa035e
Rodney Golpe <rodney+git@golpenet.com>
2016-08-16 10:15:14 -0400

    Show real datetimes in foo log and a scroll fix.

--- natelogg-test.sh
+++ natelogg-test.sh
@@ -3,7 +3,8 @@
 i=0;
 
 while [ true ]; do
-    echo '{"name":"API","hostname":"c9804837f0b1","pid":3572,"level":30,"count":'$i',"metric":"rss","RSS":105,"elapsedTime":180,"MemFreed":-0.03,"msg":"Ran Garbage Collector.","time":"2016-08-12T17:54:42.231Z","v":0}' >> /log/foo.log
+	now=$(date)
+    echo '{"name":"API","hostname":"c9804837f0b1","pid":3572,"level":30,"count":'$i',"metric":"rss","RSS":105,"elapsedTime":180,"MemFreed":-0.03,"msg":"Ran Garbage Collector.","time":"'$now'","v":0}' >> /log/foo.log
     i=$((i+1));
-    sleep 1;
+    sleep .1;
 done
