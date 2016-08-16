caedcede942dbc943f439231e0989fff21f4dc9c
Rodney G <rodney@golpenet.com>
2016-08-16 11:24:40 -0400

    Put sleep back to 1 instead of .1.

--- natelogg-test.sh
+++ natelogg-test.sh
@@ -6,5 +6,5 @@ while [ true ]; do
 	now=$(date)
     echo '{"name":"API","hostname":"c9804837f0b1","pid":3572,"level":30,"count":'$i',"metric":"rss","RSS":105,"elapsedTime":180,"MemFreed":-0.03,"msg":"Ran Garbage Collector.","time":"'$now'","v":0}' >> /log/foo.log
     i=$((i+1));
-    sleep .1;
+    sleep 1;
 done
